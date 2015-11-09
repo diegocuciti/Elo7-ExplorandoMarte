4eb569b6b3a1bcefcae9efd7e0b11a6c399ffa1f
Diego Armelin Cuciti <diego.cuciti@gmail.com>
2015-11-09 14:19:13 -0200

    Atualização de código Classes: ControleFacade e Planalto

---
+++ Elo7-ExplorandoMarte/explorando-marte/src/JUnitTestes/ControleFacadeTest.java
@@ -0,0 +1,86 @@
+package JUnitTestes;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import br.com.elo7.explorandomarte.controle.ControleFacade;
+import br.com.elo7.explorandomarte.dto.Planalto;
+import junit.framework.TestCase;
+
+public class ControleFacadeTest extends TestCase {
+	
+	@Test()
+	public void testProcessarDadosUmaSondaComSucesso() {
+		String saidaEsperada = ("1 3 N\n");
+		
+		String planalto = ("5 5");
+		String coordenadas = ("1 2 N\nLMLMLMLMM");
+		
+		ControleFacade controle = new ControleFacade();
+		String retorno = controle.processarDados(planalto, coordenadas);
+		
+		assertEquals(saidaEsperada, retorno);
+	}
+	
+	@Test()
+	public void testProcessarDadosListaSondaComSucesso() {
+		String saidaEsperada = ("1 3 N\n5 1 E\n");
+		
+		String planalto = ("5 5");
+		String coordenadas = ("1 2 N\nLMLMLMLMM\n3 3 E\nMMRMMRMRRM");
+		
+		ControleFacade controle = new ControleFacade();
+		String retorno = controle.processarDados(planalto, coordenadas);
+		
+		assertEquals(saidaEsperada, retorno);
+	}
+	
+	@Test()
+	public void testProcessarDadosUmaSondaComErro() {
+		String saidaEsperada = ("1 3 N");
+		
+		String planalto = ("5 5");
+		String coordenadas = ("1 2 N\nLMLMLMLM");
+		
+		ControleFacade controle = new ControleFacade();
+		String retorno = controle.processarDados(planalto, coordenadas);
+		
+		Assert.assertNotEquals(saidaEsperada, retorno);
+	}
+	
+	@Test()
+	public void testProcessarDadosListaSondaComErro() {
+		String saidaEsperada = ("1 3 N\n5 1 N");
+		
+		String planalto = ("5 5");
+		String coordenadas = ("1 2 N\nLMLMLMLMM\n3 3 \nMMRMMRMRRM");
+		
+		ControleFacade controle = new ControleFacade();
+		String retorno = controle.processarDados(planalto, coordenadas);
+		
+		Assert.assertNotEquals(saidaEsperada, retorno);
+	}
+	
+	@Test
+	public void testRecuperarPlanaltoComSucesso() {
+		int saidaEsperadaPlanaltoX = (5);
+		int saidaEsperadaPlanaltoY = (5);
+		
+		String planalto = ("5 5");
+		
+		Planalto planaltoRetorno = new Planalto(planalto);
+		
+		assertEquals(saidaEsperadaPlanaltoX, planaltoRetorno.getCoordenadaPontoSuperiorX());
+		assertEquals(saidaEsperadaPlanaltoY, planaltoRetorno.getCoordenadaPontoSuperiorY());
+	}
+	
+	@Test(expected=ArrayIndexOutOfBoundsException.class)
+	public void testArrayIndexOutOfBoundsExceptionComSucesso() throws ArrayIndexOutOfBoundsException {
+		
+		String planalto = ("5 ");
+		String coordenadas = ("1 2 N\nLMLMLMLMM\n");
+		
+		ControleFacade controle = new ControleFacade();
+		controle.processarDados(planalto, coordenadas);
+	}
+}
